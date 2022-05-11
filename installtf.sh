82e3660da3926c7ca0484f66e94b1f4cdbffe4c4
Desmond Lim <dkelim1@gmail.com>
2022-05-12 00:57:51 +0800

    update installtf.sh

--- installtf.sh
+++ installtf.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-su - -c 'apt-get update && apt-get install -y gnupg software-properties-common wget'
+apt-get update && apt-get install -y gnupg software-properties-common wget
 
 echo "Installing tfswitch locally"
 wget https://raw.githubusercontent.com/warrensbox/terraform-switcher/release/install.sh  #Get the installer on to your machine
