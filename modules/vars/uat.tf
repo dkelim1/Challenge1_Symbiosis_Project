3b9ce865f0078801ddcc88ede94cc14bd090a328
Desmond Lim <dkelim1@gmail.com>
2022-05-13 09:52:43 +0800

    test ASG

--- modules/vars/uat.tf
+++ modules/vars/uat.tf
@@ -2,7 +2,7 @@
 locals {
     uat = {
         apps_instance_type = "t2.micro",
-        target_value = 70.0,
+        target_value = 20.0,
         min_size = 2,
         max_size = 4,
         db_allocated_storage = 10,
